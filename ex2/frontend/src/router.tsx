82f79f86717790fd0228036ffe69a3d1671db18e
Guilherme Possebon <gpossebon67@gmail.com>
2024-11-14 13:30:58 -0300

    Remoção do login e register de usuario

--- ex2/frontend/src/router.tsx
+++ ex2/frontend/src/router.tsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./layout/DefaultLayout";
 import { Home } from "./pages/Home";
-import { Login } from "./pages/Login";
-import { Register } from "./pages/Register/intex";
 import { Return } from "./pages/Return";
 import { Create } from "./pages/Create";
 
@@ -12,8 +10,6 @@ export function Router() {
       <Routes>
         <Route path="/" element={<DefaultLayout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
           <Route path="/return" element={<Return />} />
           <Route path="/create" element={<Create />} />
         </Route>
